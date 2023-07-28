fdc7361c108121d066cb47c660219f9905361322
Shmuel Amir <shmulik5777@gmail.com>
2023-07-28 18:55:30 +0300

    user based

--- exe.R
+++ exe.R
@@ -8,8 +8,9 @@ library(crayon)
 
 # Load the data from the saved file
 load("recommender3.rdata")
+# pdf("recommender.pdf")
 
-# 0 - unread book
+
 # ---------- Clean the data ---------- #
 
 # remove unneeded columns
@@ -40,6 +41,7 @@ books <- books %>% semi_join(ratings, by = "ISBN")
 
 ratings$`User-ID` <- as.factor(ratings$`User-ID`)
 ratings$ISBN <- as.factor(ratings$ISBN)
+ratings <- ratings %>% mutate(`Book-Rating` = if_else(`Book-Rating` == 0, NA, `Book-Rating`)) # maybe remove because the calculations
 
 # remove duplicate rows
 ratings <- ratings %>% distinct(`User-ID`, `ISBN`, .keep_all = TRUE)
@@ -64,145 +66,47 @@ UI <- new("realRatingMatrix", data = M)
 rm(M)
 gc()
 
-slotNames(UI)
-head(names(colCounts(UI)))
-
-vector_ratings <- as.vector(UI@data)
-
-# remove zeros
-vector_ratings <- vector_ratings[vector_ratings != 0]
-table(vector_ratings)
-hist(vector_ratings, main="Histogram of Ratings", xlab="Rating Value")
-
-
-ratings <- UI[rowCounts(UI) > 50, colCounts(UI) > 100]
-dim(ratings)
-
-
-#  -----------------return 5 ISBN books for each user - example on small data
-# train <- UI[1:1000]
-# rec <- Recommender(train, method = "UBCF")
-# pre <- predict(rec, UI[1001:1002], n = 5)
 # 
-# similarity(UI[1:50,], method = "cosine", which = "users", min_matching = 10)
+# vector_ratings <- as.vector(UI@data)
 # 
-# scheme <- evaluationScheme(
-#   UI[1:1000], 
-#   method = "cross-validation", 
-#   k = 10, 
-#   given = -5, 
-#   goodRating = 7)
+# # remove zeros
+# vector_ratings <- vector_ratings[vector_ratings != 0]
+# table(vector_ratings)
+# hist(vector_ratings, main="Histogram of Ratings", xlab="Rating Value")
 
 
+# removing less than 3 quantile
+row.threshold <- quantile(rowCounts(UI))[[2]]
+col.threshold <- quantile(colCounts(UI))[[4]]
+UI.ratings <- UI[
+  rowCounts(UI) >= row.threshold, 
+  colCounts(UI) >= col.threshold
+]
+dim(UI.ratings@data)
 
+# normalize the data
+UI.ratings.n <- normalize(UI.ratings)
+UI.ratings.n.vec <- as.vector(UI.ratings.n@data)
+UI.ratings.n.vec <- UI.ratings.n.vec[UI.ratings.n.vec != 0]
 
-
-
-#  ----------------- from the lab
-# R <- rle(colSums(M>0))
-# 
-# thresh <- 3
-# S <- sort(R$lengths[R$values > thresh], decreasing = TRUE)
-# 
-# run <- S[1]
-# loc <- grep(names(S[1]), colnames(M))
-# 
-# nonzero.rows <- unique(sort(as.vector(
-#   apply(M[, (loc-run):(loc-1)], MARGIN = 2, FUN = function(e) which(e>0))
-# )))
-# 
-# M[nonzero.rows, (loc-run):(loc-1)]
-# 
-# ratings.n <- normalize(UI)
-# ratings.n.vec <- as.vector(ratings.n@data)
+hist(UI.ratings.n.vec, main = "Histogram of Normalized Ratings", xlab = "Rating")
 
 
 # ---------- The model ---------- #
 
 # Train a model with holdout (k-fold)
 percent_train <- 0.8
-given <- 2
-goodRating <- 5 
+given <- 15
+goodRating <- 5 # check maybe another number
 k <- 1           
 
 eval_sets <- evaluationScheme(
-  data = UI, method = "split",
+  data = UI.ratings, method = "split",
   train = percent_train, given = given,
   goodRating = goodRating, k = k
 )
 
-# save(eval_sets, UI, M, nonzero.rows, R, ratings, file = "recommender3.rdata")
-
-# example from a tutorial
-# model_train_scheme <- UI %>%
-#   evaluationScheme(method = 'split', 
-#                    train = 0.8, 
-#                    given = 2,
-#                    goodRating = 6,
-#                    k = 1)
-# 
-# model_params <- list(method = "cosine",
-#                      nn = 10, # find each user's 10 most similar users.
-#                      sample = FALSE, # already did this.
-#                      normalize = "center")
-# 
-# model1 <- getData(model_train_scheme, "train") %>% 
-#   Recommender(method = "UBCF", parameter = model_params)
-# 
-# model1_pred <- predict(model1, getData(model_train_scheme, "known"), type = "ratings")
-
-
-# chatGPT
-
-
-batch_size <- 505
-
-num_rows <- nrow(UI)
-num_batches <- ceiling(num_rows / batch_size)
-
-all_predictions <- vector("list", num_batches)
-start <- Sys.time()
-print(num_batches)
-
-for (i in 1:num_batches) {
-  start_row <- (i - 1) * batch_size + 1
-  end_row <- min(i * batch_size, num_rows)
-  
-  current_batch <- UI[start_row:end_row, ]
-  
-  model <- Recommender(current_batch, method = "UBCF")
-  
-  predictions <- predict(model, current_batch)
-  
-  all_predictions[[i]] <- predictions
-  
-  end <- Sys.time()
-  print(paste("done for batch: ", i, "in ", end - start))
-  start <- end
-}
-
-final_predictions <- do.call(rbind, all_predictions)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # User Based
-
 system.time(
   eval_recommender <- Recommender(
     data = getData(eval_sets, "train"),
@@ -210,42 +114,9 @@ system.time(
   )
 )
 
+# ---------- Predictions (for all users): ---------- #
 
-
-# Item Based
-
-# system.time(
-#   eval_recommender <- Recommender(
-#     data = getData(eval_sets, "train"),
-#     method = "IBCF", parameter = NULL # method = IBCF
-#   )
-# )
-
-
-# similarity matrix - calculate diff between users שתי מטריצות אחת למשתמשים ואחת לספרים
-model <- eval_recommender@model
-# model$weighted <- FALSE
-
-normalize_data <- normalize(getData(eval_sets, "known"), method = model$normalize)
-
-# U <- similarity(UI[1:4,], method = "cosine", which = "users")
-U <- similarity(normalize_data, model$data, model$method,
-                min_matching = model$min_matching_items,
-                min_predictive = model$min_predictive_items)
-  
-# I <- similarity(
-#     newdata, model$data, method = model$method, min_matching = model$min_matching_items, 
-#     min_predictive = model$min_predictive_items
-# )
-  
-
-# check the model by confusion matrix????????????
-# show the confusion matrix from recommender lab - sum = num of non zero ratings
-# roc - reciver operating rate, calc from the matrix - לשחק עם החותך מה נחשב טוב ומה רע
-#    המהתנה goodRating  <- 5 
-
-# ---------- Predictions (for all users): ---------- # לעשות בחלקים לפי חלקים של מטריצת הדמיון
-
+# get user recommendations for 500 books
 items_to_recommend <- 10
 system.time(
   eval_prediction <- predict(
@@ -256,7 +127,49 @@ system.time(
   )
 )
 
-# get user recommendations for 500 books
+
+eval_accuracy <- calcPredictionAccuracy(x = eval_prediction,
+                                        data = getData(eval_sets, "unknown"),
+                                        byUser = TRUE)
+head(eval_accuracy)
+
+save(eval_accuracy, eval_prediction, eval_recommender, UI.ratings, ratings, file = "recommender3.rdata")
+rm(eval_accuracy, eval_prediction, eval_recommender, UI.ratings, ratings)
+rm(UI.ratings.n, UI.ratings.n.vec, UI)
+gc()
+
+
+# Item Based
+
+system.time(
+  item_eval_recommender <- Recommender(
+    data = getData(eval_sets, "train"),
+    method = "IBCF", parameter = NULL
+  )
+)
+
+system.time(
+  item_eval_prediction <- predict(
+    object = item_eval_recommender,
+    newdata = getData(eval_sets, "known"),
+    n = items_to_recommend,
+    type = "ratings"
+  )
+)
+
+item_eval_accuracy <- calcPredictionAccuracy(x = item_eval_prediction,
+                                        data = getData(eval_sets, "unknown"),
+                                        byUser = TRUE)
+head(item_eval_accuracy)
+
+
+
+
+# check the model by confusion matrix????????????
+# show the confusion matrix from recommender lab - sum = num of non zero ratings
+# roc - reciver operating rate, calc from the matrix
+
+
 
 # get new book ISBN
 
@@ -265,8 +178,5 @@ system.time(
 
 
 
-
-
-
-
+# get the titles of the books
 
